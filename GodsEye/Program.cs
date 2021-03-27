1dad31c5b0b8ffb540b9b6ef5ac483a544e0bf2d
edionu97 <onu_edy@yahoo.com>
2021-03-27 07:28:00 +0200

    created the solution + added git ignore

---
+++ GodsEye/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodsEye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
