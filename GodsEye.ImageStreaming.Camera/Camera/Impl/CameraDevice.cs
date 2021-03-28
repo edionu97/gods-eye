1c9c1ae386ee44c5795b1230056f9c820c254516
edionu97 <onu_edy@yahoo.com>
2021-03-28 18:39:24 +0300

    started camera implementation

--- GodsEye.ImageStreaming.Camera/Camera/Impl/CameraDevice.cs
+++ GodsEye.ImageStreaming.Camera/Camera/Impl/CameraDevice.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using GodsEye.Utility.Configuration;
 using GodsEye.ImageStreaming.ImageSource.ImageProvider;
@@ -16,10 +18,49 @@ namespace GodsEye.ImageStreaming.Camera.Camera.Impl
             _applicationSettings = applicationSettings;
         }
 
-        public Task StartTackingShots(int deviceId)
+        public Task StartTackingShots(string deviceId, int devicePort)
         {
+            //get the starting point and the camera address
+            var (_, _, cameraAddress) = _applicationSettings.Camera.Network;
 
-            throw new NotImplementedException();
+            //create a new task
+            return Task.Factory.StartNew(() =>
+            {
+                //parse the address 
+                var ipAddress = IPAddress.Parse(cameraAddress);
+
+                //get the listener
+                var clientListener = new TcpListener(ipAddress, devicePort);
+
+                //start the client listener
+                clientListener.Start();
+
+                //display the message
+                Console.WriteLine($"Camera {deviceId} is streaming images on port {deviceId}...");
+
+                //wait for connections
+                while (true)
+                {
+                    //get the client socket
+                    var client = clientListener.AcceptSocket();
+
+                    //process the message socket
+                    OnClientConnected(client, deviceId);
+                }
+
+                // ReSharper disable once FunctionNeverReturns
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        private void OnClientConnected(Socket client, string deviceId)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                await foreach (var (imageName, imageBytes) in _imageProvider.ProvideImages(deviceId))
+                {
+
+                }
+            });
         }
     }
 }
--- GodsEye.Utility/Configuration/Sections/Camera/NetworkSection.cs
+++ GodsEye.Utility/Configuration/Sections/Camera/NetworkSection.cs
@@ -5,12 +5,12 @@ namespace GodsEye.Utility.Configuration.Sections.Camera
     public class NetworkSection
     {
         public ImageType ImageStreamingFormat { get; set; }
-        public  string StreamsOnPort { get; set; }
+        public int StreamsOnPort { get; set; }
         public string CamerasLocation { get; set; }
 
         public void Deconstruct(
             out ImageType type, 
-            out string port,
+            out int port,
             out string camerasLocation)
         {
             port = StreamsOnPort;
